29080a0e340187c77867d2ca5fe1936484487c3e
kimbob9108 <kimbob9108@naver.com>
2019-05-27 12:12:05 +0900

    메인 이미지 쿼리 추가

--- js/BOBSANG_main.js
+++ js/BOBSANG_main.js
@@ -1,4 +1,4 @@
-window.onload=function(){
+/*window.onload=function(){
             document.getElementsByClassName('left_mini_img')[0].addEventListener('mouseover',function(){
                 document.getElementsByClassName('main_big_img').setAttribute('src','img/discount-1015446_1280.jpg');
                 this.setAttribute('alt','세일');
@@ -26,4 +26,19 @@ window.onload=function(){
                 document.getElementsByClassName('main_big_img').setAttribute('src','img/farmer-3431359_1280.jpg');
                 this.setAttribute('alt','농사');
             });
-}
+}*/
+
+$(document).ready(function(){
+    let mainimgsrc;
+    let mainimgclass = $('.main_big_img');
+   let leftimg=$('.left_black_box').find('img');
+    leftimg.on('mouseover',function(){
+       let src = $(this).attr('src');
+        mainimgsrc = mainimgclass.attr('src');
+        mainimgclass.attr('src',src);
+    });
+
+    leftimg.on('mouseout',function(){
+        mainimgclass.attr('src',mainimgsrc);
+    })
+});
