6f8e95db88115ea2f4b55850f6a595554787a56a
kimbob9108 <kimbob9108@naver.com>
2019-05-28 15:39:37 +0900

    알러지 쿼리 수정중 입니다.

--- js/BOBSANG_ALLERGY.js
+++ js/BOBSANG_ALLERGY.js
@@ -1,21 +1,28 @@
-$(document).ready(function() {
-            $('.allergylist').find('td').on('dblclick', function() {
-                var searchname = $(this).text();
-                $('#allergy').find('td').each(function(index, element) {
-                    let tmpname = $(element).text();
-                    if (tmpname.indexOf(searchname) >= 0) {
-                        this.style.backgroundColor = 'red';
-                    }
-                });
+$(document).ready(function () {
+    $('.allergylist').find('td').on('click', function () {
+        let list_this = this;
+        let searchname = $(list_this).text();
+        if ($(list_this).css('background-color') == 'rgb(255, 0, 0)') {
+            $('#allergy').find('td').each(function (index, element) {
+                let tmpname = $(element).text();
+                if (tmpname.indexOf(searchname) >= 0) {
+                    this.style.backgroundColor = 'rgba(0, 0, 0, 0)';
+                }
             });
+        } else {
+            $('#allergy').find('td').each(function (index, element) {
+                let tmpname = $(element).text();
+                if (tmpname.indexOf(searchname) >= 0) {
+                    this.style.backgroundColor = 'rgb(255, 0, 0)';
+                }
+            });
+        }
 
-            $('.allergylist').find('td').on('click', function() {
-                var searchname = $(this).text();
-                $('#allergy').find('td').each(function(index, element) {
-                    let tmpname = $(element).text();
-                    if (tmpname.indexOf(searchname) >= 0) {
-                        this.style.backgroundColor = 'rgb(229, 220, 203)';
-                    }
-                });
-            })
+        $('#allergy').find('td').each(function (index, element) {
+            let tmpname = $(element).text();
+            if (tmpname.indexOf(searchname) >= 0) {
+                this.style.backgroundColor = 'rgba(0, 0, 0, 0)';
+            }
         });
+    });
+});
