4021468038b7db65ed26cb5be35193e7d753cf8d
kimbob9108 <kimbob9108@naver.com>
2019-05-28 18:00:37 +0900

    알레르기 강사님 피드백 개선  했습니다.

--- js/BOBSANG_ALLERGY.js
+++ js/BOBSANG_ALLERGY.js
@@ -3,15 +3,23 @@ $(document).ready(function () {
         let list_this = this;
         let searchname = $(list_this).text();
         let color;
-        if ($(list_this).css('background-color') == 'rgb(255, 0, 0)') {
+        let fontweight;
+        let fontsize;
+        if ($(list_this).css('background-color') == 'rgba(0, 191, 255, 0.5)') {
             color = 'rgba(0, 0, 0, 0)';
+            fontweight = 'lighter';
+            fontsize = '20px';
         } else {
-            color = 'rgb(255, 0, 0)';
+            color = 'rgba(0, 191, 255, 0.5)';
+            fontweight = 'bold';
+            fontsize = '30px';
         }
         $('#allergy').find('td').each(function (index, element) {
             let tmpname = $(element).text();
             if (tmpname.indexOf(searchname) >= 0) {
                 this.style.backgroundColor = color;
+                this.style.fontWeight = fontweight;
+                this.style.fontSize = fontsize;
             }
         });
     });
