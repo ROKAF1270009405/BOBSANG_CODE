b43dd715b0b65165a1a93b0e313f2ba9ac525450
kimbob9108 <kimbob9108@naver.com>
2019-05-28 16:03:34 +0900

    min 크기 삭제 및 모바일테이블 외곽선 좌우 제거 합니다.

--- js/BOBSANG_ALLERGY.js
+++ js/BOBSANG_ALLERGY.js
@@ -2,26 +2,16 @@ $(document).ready(function () {
     $('.allergylist').find('td').on('click', function () {
         let list_this = this;
         let searchname = $(list_this).text();
+        let color;
         if ($(list_this).css('background-color') == 'rgb(255, 0, 0)') {
-            $('#allergy').find('td').each(function (index, element) {
-                let tmpname = $(element).text();
-                if (tmpname.indexOf(searchname) >= 0) {
-                    this.style.backgroundColor = 'rgba(0, 0, 0, 0)';
-                }
-            });
+            color = 'rgba(0, 0, 0, 0)';
         } else {
-            $('#allergy').find('td').each(function (index, element) {
-                let tmpname = $(element).text();
-                if (tmpname.indexOf(searchname) >= 0) {
-                    this.style.backgroundColor = 'rgb(255, 0, 0)';
-                }
-            });
+            color = 'rgb(255, 0, 0)';
         }
-
         $('#allergy').find('td').each(function (index, element) {
             let tmpname = $(element).text();
             if (tmpname.indexOf(searchname) >= 0) {
-                this.style.backgroundColor = 'rgba(0, 0, 0, 0)';
+                this.style.backgroundColor = color;
             }
         });
     });
